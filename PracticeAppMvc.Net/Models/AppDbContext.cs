841044423e14cf5799ac581b8343aa9ba8dc48a9
truongkienan <truongkienan2@gmail.com>
2023-07-01 16:30:18 +0700

    kết thúc thực hành buổi 3, entity framework

---
+++ PracticeAppMvc.Net/Models/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PracticeAppMvc.Net.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            base.OnConfiguring(builder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
